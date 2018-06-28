407c11cbb3f5887b8b6a330eb7c457b7fb3b4e8e
ITO MINORU <mito@mikilab.doshisha.ac.jp>
2018-06-28 09:30:03 +0900

    first

---
+++ server_client/public/update.js
@@ -0,0 +1,24 @@
+$(function() {
+    var POLLLING_INVERVAL_TIME_IN_MILLIS = 5000;//5.0s
+    (function polling() {
+        getSmellUpdate();
+        window.setTimeout(polling, POLLLING_INVERVAL_TIME_IN_MILLIS);
+        //console.log(req.body[0].smell);
+      }());
+
+    function getSmellUpdate() {
+      $.ajax({
+      type : "GET",
+      url : "smellUpdate",
+      content : "application/json",
+      dataType : "json",
+    }).done(function(data) {
+      console.log(data.smell);
+      var txt = "臭い：" + data.smell;
+      document.getElementById("smellTxt").innerHTML = txt;
+      //$("dd").text(data.count);//html要素変更する
+    }).fail(function(jqXHR, textStatus) {
+      $("dd").text("error occured");//html要素変更する
+      });
+    }
+  });
